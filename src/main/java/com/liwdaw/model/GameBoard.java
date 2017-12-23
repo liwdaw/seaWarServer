4f61608379ae44f3d97834c8df43469d1882aeca
liwdaw <liwocha.dawid@gmail.com>
2017-12-23 12:09:06 +0100

    implemented more methods for GameBoard

--- src/main/java/com/liwdaw/model/GameBoard.java
+++ src/main/java/com/liwdaw/model/GameBoard.java
@@ -8,8 +8,14 @@ import java.util.Map;
 public class GameBoard implements Serializable {
 	
 	private Map<Coordinates, Integer> battleGround;		// -1 = miss; 1 = hit
+	private List<Ship> ships;
 	private int xSize = 10;
 	private int ySize = 10;
+	
+	public GameBoard(List<Ship> ships) {
+		this.ships = ships;
+		initBattleGround();
+	}
 	
 	public GameBoard(Map<Coordinates, Integer> battleGround) {
 		this.battleGround = battleGround;
@@ -24,11 +30,23 @@ public class GameBoard implements Serializable {
 	}
 	
 	private void initBattleGround() {
+		battleGround = new HashMap<Coordinates, Integer>(100);
 		for (int i=0; i<xSize; i++) {
 			for (int j=0; j<ySize; j++) {
 				battleGround.put(new Coordinates(i, j), 0);
 			}
 		}
+	}
+	
+	public void shoot(Coordinates coordinates) {
+		for (int i=0; i<ships.size(); i++) {
+			if (ships.get(i).areThisShipCoordinates(coordinates)) {
+				ships.get(i).destroyPart(coordinates);
+				battleGround.put(coordinates, 1);
+				return;
+			}
+		}
+		battleGround.put(coordinates, -1);
 	}
 	
 }
